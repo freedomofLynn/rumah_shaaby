32be9f71cb8b5607394023da281afcd87b7f53a7
Ridat Maulana <ridatmaulana28@gmail.com>
2023-03-13 12:32:46 +0700

    migrasi data customer

---
+++ database/migrations/2023_03_13_045144_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->uuid('customer_id')->primary();
+            $table->string('firstname', 50);
+            $table->string('lastname', 50);
+            $table->text('address');
+            $table->string('poscode', 5);
+            $table->string('telephone', 14);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
